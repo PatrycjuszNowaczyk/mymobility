580b03589908be5611e956b1fc9c8bb1d3414d3e
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2025-05-20 22:28:05 +0200

    feat(warunkowe-wyswietlanie-pytan): add conditional logic for step 4_2 questions
    expand functionality to hide and reset values for specific questions based on checkbox state in step 4_2.
    
    Signed-off-by: Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>

--- src/js/badania/warunkowe-wyswietlanie-pytan.js
+++ src/js/badania/warunkowe-wyswietlanie-pytan.js
@@ -1,6 +1,5 @@
-// KROK1_1 START
 ( function () {
-  const questions_to_hide_in_step_2 = [
+  const questions_to_hide_in_step_1_1 = [
     "KROK1_1_37",
     "KROK1_1_40",
     "KROK1_1_43",
@@ -31,6 +30,19 @@
     "KROK1_1_96",
   ]
 
+  const questions_to_hide_in_step_4_2 = [
+    "KROK4_2_5",
+    "KROK4_2_6",
+    "KROK4_2_7",
+    "KROK4_2_8",
+    "KROK4_2_9",
+    "KROK4_2_10",
+    "KROK4_2_11",
+    "KROK4_2_12",
+    "KROK4_2_13",
+    "KROK4_2_14"
+  ];
+
   const types_of_input_to_reset = [
     'input[type="text"]',
     'input[type="number"]',
@@ -44,14 +56,16 @@
     'input[type="checkbox"]:checked'
   ]
 
-  console.log( 'warunkowe wyswietlanie' );
+
+//----------------------------------------------------------------------------------------------------------------------
 
   $( document ).ready( function () {
+// KROK1_1 START
     $( document ).on( 'change', 'input[type="radio"][name="KROK1_1_32"]:checked', function () {
       if ( $( this ).val() === '3' || $( this ).val() === '4' ) {
         ( function hide_questions_to_hide_in_step_2() {
           $( '.item' ).each( function () {
-            if ( questions_to_hide_in_step_2.includes( $( this ).attr( 'data-item-id' ) ) ) {
+            if ( questions_to_hide_in_step_1_1.includes( $( this ).attr( 'data-item-id' ) ) ) {
               if ( types_of_input_to_reset.some( type => $( this ).find( type ).val() !== undefined ) ) {
                 types_of_input_to_reset.forEach( type => {
                   $( this ).find( type ).val( '' );
@@ -69,13 +83,47 @@
         } )()
       } else {
         $( '.item' ).each( function () {
-          if ( questions_to_hide_in_step_2.includes( $( this ).attr( 'data-item-id' ) ) ) {
+          if ( questions_to_hide_in_step_1_1.includes( $( this ).attr( 'data-item-id' ) ) ) {
             $( this ).removeClass( 'hide' );
             $( this ).show();
           }
         } )
       }
     } )
+// KROK1_1 END
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// KROK4_2 START
+    $( document ).on( 'change', 'input[type="checkbox"][name="KROK4_2_4[]"][value="1.no_statement"]', function () {
+      if ( $( this ).is( ':checked' ) ) {
+        ( function hide_questions_to_hide_in_step_2() {
+          $( '.item' ).each( function () {
+            if ( questions_to_hide_in_step_4_2.includes( $( this ).attr( 'data-item-id' ) ) ) {
+              if ( types_of_input_to_reset.some( type => $( this ).find( type ).val() !== undefined ) ) {
+                types_of_input_to_reset.forEach( type => {
+                  $( this ).find( type ).val( '' );
+                } );
+              }
+
+              if ( types_of_input_to_uncheck.some( type => $( this ).find( type ).is( ':checked' ) ) ) {
+                types_of_input_to_uncheck.forEach( type => $( this ).find( type ).prop( 'checked', false ) );
+              }
+
+              $( this ).addClass( 'hide' );
+              $( this ).hide();
+            }
+          } )
+        } )()
+      } else {
+        $( '.item' ).each( function () {
+          if ( questions_to_hide_in_step_4_2.includes( $( this ).attr( 'data-item-id' ) ) ) {
+            $( this ).removeClass( 'hide' );
+            $( this ).show();
+          }
+        } )
+      }
+    } )
+// KROK4_2 END
   } )
 } )();
-// KROK1_1 END
